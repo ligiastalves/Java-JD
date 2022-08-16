d2cd07148a916ab0a9efcd298a69657b9cfe1e7d
Ligia Alves <ligia.stalves@gmail.com>
2022-08-16 12:58:45 -0300

    inicio
    usando metodos

---
+++ src/main/java/ExemploMetodoMain.java
@@ -0,0 +1,8 @@
+public class ExemploMetodoMain {
+
+    public static void main (String[] args){
+
+        System.out.println("Metodo main executando...");
+    }
+
+}
---
+++ src/main/java/HelloWorldMetodos.java
@@ -0,0 +1,18 @@
+public class HelloWorldMetodos {
+
+    static void olaMetodos(){
+        //definindo um metodo que exibe um msg simples
+        System.out.println("Sou um novo metodo!");
+
+    }
+
+    public static void main (String[] args){
+        System.out.println("Começando a execução do programa.");
+
+        //momento que o metodo é chamado, colocado em execucao
+        olaMetodos();
+
+        System.out.println("Terminando a execução do programa.");
+    }
+}
+
