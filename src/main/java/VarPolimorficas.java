e8ca39db782bd1d4eb0b7082b5aa135da8d21ff0
Ligia Alves <ligia.stalves@gmail.com>
2022-08-23 09:54:18 -0300

    polimorfismo

---
+++ src/main/java/VarPolimorficas.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VarPolimorficas {
+
+    //Uma variável polimórfica é uma variável de referência capaz de referenciar
+    // diferentes (múltiplos, daí o seu nome) tipos de objetos.
+    class Animal{
+        public void fazerBarulho(){
+            System.out.println("Animal fazendo barulho");
+        }
+    }
+    class Gato extends Animal{
+        public void fazerBarulho(){
+            System.out.println("Miau");
+        }
+    }
+    class Cachorro extends Animal{
+        public void fazerBarulho(){
+            System.out.println("Au Au");
+        }
+    }
+    public class Polimorfismo{
+        public static void main(String[] args){
+            List animais = new ArrayList<>();
+            animais.add(new Gato());
+            animais.add(new Gato());
+            animais.add(new Cachorro());
+            for (Animal animal : animais)
+            //polimorfismo dinamico aqui
+            //em tempo de execucao, a JVM decide qual
+            //metodo chamar em funcao do tipo do objeto
+            animal.fazerBarulho();
+
+        }
+    }
+}
